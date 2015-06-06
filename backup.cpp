891dfd83c62067049c53c340069a27817f6b5881
thejonathanwong <jonathanmxwong@gmail.com>
2015-06-06 14:30:55 -0700

    adding functionality for directory as input

--- backup.cpp
+++ backup.cpp
@@ -14,24 +14,30 @@
 int main(int argc, char * argv[]) {
 
 	if(argc != 3) {
-//		cerr << "USAGE: " << argv[0] << " <Input File> <Output Directory> \n";
+		//		cerr << "USAGE: " << argv[0] << " <Input File> <Output Directory> \n";
 		fprintf(stderr, "USAGE: %s <Input File> <Output Directory>\n", argv[0]);
 		exit(1);
 	}
 
 	char * filename = argv[1];
-//	string filename {argv[1]};
 	char * dirname = argv[2];
-	string dirString {dirname};
+
+	//checks input file and opens
+	int fd;
+	if((fd = open(filename, O_RDONLY,0)) < 0) {
+		fprintf(stderr, "Unable to open input file\n");
+		exit(1);
+	}
 
 	//checks to see if output directory exists
 	DIR * dir;
 	if(NULL == (dir = opendir(dirname))) {
-		fprintf(stderr, "Cannot open directory %s\n", dirname);
+		fprintf(stderr, "Cannot open directory \"%s\"\n", dirname);
 		exit(1);
 	}
 
 
+	string dirString {dirname};
 	md5 hasher;
 	unordered_set<string> filesFound;
 
@@ -57,7 +63,7 @@ int main(int argc, char * argv[]) {
 		//hashes the file found and inserts into the hashet
 		hasher.hash(fileFound);
 		filesFound.insert(hasher.toHex());
-//		cout << "Hash of " << file->d_name << ": " << hasher.toHex() << "\n";
+		//		cout << "Hash of " << file->d_name << ": " << hasher.toHex() << "\n";
 
 		//closes the file
 		fileFound.close();
@@ -70,63 +76,64 @@ int main(int argc, char * argv[]) {
 
 
 
-	//opens input file
-	int fd;
-	if((fd = open(filename, O_RDONLY, 0)) > 0) {
-//		FILE * infile = fdopen(fd, "r");
 
-		struct stat statBuffer;
-		fstat(fd, &statBuffer);
 
-//		hasher.hash(infile);
-		hasher.hash(fd);
-//		cout << "hash of " << filename << ": " << hasher.toHex() << "\n";
+	struct stat statBuffer;
+	if(fstat(fd, &statBuffer) == 0) {
 
-		//if file is not in the hashset, then write the file to the output director
-		if(filesFound.count(hasher.toHex()) == 0) {
-			
-			//resets error flag and moves pointer to start of the file
-//			clearerr(infile);
-//			fseek(infile, 0, SEEK_SET);
-			lseek(fd, 0, SEEK_SET);
+		if( statBuffer.st_mode & S_IFDIR ) {
 
-			//creates destination file and writes to it
-			string pathString = dirString + "/" + filename;
-			const char * path = pathString.c_str();
-			int dest;
-			if( (dest = open(path, O_WRONLY | O_CREAT, 0644)) > 0) {
 
-				//uses Linux kernel to copy the file so NOT PORTABLE TO OTHER OS
-				if(sendfile(dest, fd, 0, statBuffer.st_size) != statBuffer.st_size) {
-					fprintf(stderr, "Did not copy %s correctly\n", path);
+			cout << "Directory\n";
+
+
+		} else if ( statBuffer.st_mode & S_IFREG ) {
+			//input is a file
+			cout << "File\n";
+
+
+
+			//		hasher.hash(infile);
+			hasher.hash(fd);
+			//		cout << "hash of " << filename << ": " << hasher.toHex() << "\n";
+
+			//if file is not in the hashset, then write the file to the output director
+			if(filesFound.count(hasher.toHex()) == 0) {
+
+				//resets error flag and moves pointer to start of the file
+				lseek(fd, 0, SEEK_SET);
+
+				//creates destination file and writes to it
+				string pathString = dirString + "/" + filename;
+				const char * path = pathString.c_str();
+				int dest;
+				if( (dest = open(path, O_WRONLY | O_CREAT, 0644)) > 0) {
+
+					//uses Linux kernel to copy the file so NOT PORTABLE TO OTHER OS
+					if(sendfile(dest, fd, 0, statBuffer.st_size) != statBuffer.st_size) {
+						fprintf(stderr, "Did not copy %s correctly\n", path);
+					}
+
+					close(dest);
+				} else {
+					fprintf(stderr, "Unable to open destination file %s\n", path);
 				}
-
-				close(dest);
-			} else {
-				fprintf(stderr, "Unable to open destination file %s\n", path);
 			}
 
 
-//			string path = dirString + "/" + filename;
-//			ofstream dest { path };
-//			if(dest == NULL) {
-//				fprintf(stderr, "Cannot write %s to %s\n", filename, path.c_str());
-//			} else {
-//
-//				//writes the file to the destination
-//				dest << infile.rdbuf();
-//
-//				//closes thes stream
-//				dest.close();
-//			}
-		}
 
-		close(fd);
-//		fclose(infile);
-	} else {
-		fprintf(stderr, "Unable to open input file\n");
+		} else {
+			cout << "Not file or directory\n";
+		}
 	}
-	
+
+
+
+
+
+	close(fd); //closes input file
+
+
 
 
 
